59357c12f1f98ef26e4f80f042f04ec9c8e65f57
ryuqae <ryuqae@gmail.com>
2021-07-05 18:28:13 +0900

    add new features: scheduler not yet

--- main.py
+++ main.py
@@ -1,55 +1,191 @@
 import telegram
-from telegram.ext import CommandHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import (
+    Filters,
+    CommandHandler,
+    MessageHandler,
+    Updater,
+    CallbackQueryHandler,
+    CallbackContext,
+    ConversationHandler,
+)
 
 import logging
-from apscheduler.schedulers.blocking import BlockingScheduler
 
-# import json
+# from apscheduler.schedulers.blocking import BlockingScheduler
+
 from news_search import newsUpdater
 
-# logging.basicConfig(
-#     level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-# )
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 
+# Authentication
 with open("./access_token.txt") as f:
     lines = f.readlines()
     TOKEN = lines[0].strip()
 
 bot = telegram.Bot(token=TOKEN)
-sched = BlockingScheduler()
+
+
+# chat_id = bot.getUpdates()[-1].message.chat.id  # 봇에 메시지를 보내기 위한 chat id 구하기
+
+# sched = BlockingScheduler()
 
 
 # 기존에 보냈던 링크를 담아둘 리스트
-old_links = []
+old_links = dict()
+keywords = set()
 
-updates = bot.get_updates()
+# update = bot.get_updates()
 
 # for u in updates:
 #     print(u.message["chat"]["username"], u.message["chat"])
 
-
 # chat_id = bot.getUpdates()[-1].message.chat.id  # 가장 최근에 온 메세지의 chat id를 가져옵니다
 # print(chat_id)
 
-# bot.sendMessage(chat_id=chat_id, text="안녕하세요")
+
+def get_chat_id(update, context):
+    chat_id = -1
+
+    if update.message is not None:
+        # text message
+        chat_id = update.message.chat.id
+    elif update.callback_query is not None:
+        # callback message
+        chat_id = update.callback_query.message.chat.id
+    elif update.poll is not None:
+        # answer in Poll
+        chat_id = context.bot_data[update.poll.id]
+
+    return chat_id
 
 
-def send_links():
-    global old_links
-    updater = newsUpdater("카카오")
-    print(f"saved links : {sorted(old_links)}")
-    new_links = updater.get_updated_news(old_links)
-    print(f"new links : {sorted(new_links)}\n===============\n")
+def start(update: Update, context: CallbackContext) -> None:
+    options = [
+        [
+            InlineKeyboardButton(text="키워드 추가", callback_data="1"),
+            InlineKeyboardButton(text="키워드 삭제", callback_data="2"),
+        ],
+        [
+            InlineKeyboardButton(text="현재 키워드 목록", callback_data="3"),
+            InlineKeyboardButton(text="검색주기 설정", callback_data="4"),
+        ],
+        [
+            InlineKeyboardButton(text="지금 검색", callback_data="5"),
+        ],
+    ]
 
-    if new_links:
-        bot.sendMessage(chat_id=62786931, text=f"새로운 뉴스 {len(new_links)}건")
-        for link in new_links:
-            bot.sendMessage(chat_id=62786931, text=link)
+    # Buttons' layout markup
+    reply_markup = InlineKeyboardMarkup(options)
+
+    # Message with the buttons
+    context.bot.send_message(
+        chat_id=get_chat_id(update, context),
+        text="원하는 작업이 무엇인가?",
+        reply_markup=reply_markup,
+    )
+
+    # selection from callback
+    # choice = update.callback_query.data
+    # if choice == "1":
+    #     # Choice 1: Text
+    #     update.callback_query.message.edit_text("You have chosen Text")
+
+
+def current_keyword(update: Update, context: CallbackContext):
+    print(update.message)
+    if len(keywords) == 0:
+        context.bot.send_message(get_chat_id(update, context), "키워드를 추가하세요 /start")
+
     else:
-        bot.sendMessage(chat_id=62786931, text="새로운 뉴스 없음")
-    old_links += new_links.copy()
-    old_links = list(set(old_links))
+        context.bot.send_message(
+            get_chat_id(update, context), f"현재 키워드 [{' | '.join(list(keywords))}]"
+        )
+
+
+def add_keyword(update: Update, context: CallbackContext) -> None:
+    global keywords
+    user = update.message.from_user
+    input_keyword = update.message.text.strip()
+    print(input_keyword, keywords)
+
+    if "초기화" in input_keyword.split():
+        keywords = set()
+    elif input_keyword not in keywords:
+        update.message.reply_text(f"{user.first_name}(이)가 추가할 키워드는 [{input_keyword}]")
+        keywords.add(input_keyword)
+        # Add new keyword into old_links
+        if input_keyword not in old_links.keys():
+            old_links[input_keyword] = []
+    else:
+        update.message.reply_text(f"{user.first_name}(이)가 제거할 키워드는 [{input_keyword}]")
+        keywords.remove(input_keyword)
+    current_keyword(update, context)
+
+
+# def remove_keyword(update: Update, context: CallbackContext) -> None:
+#     user = update.message.from_user
+#     update.message.reply_text(f"{user.name}(이)가 삭제할 키워드는 [{update.message.text}]")
+
+
+def button(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    choice = query.data
+    # print(query)
+
+    query.edit_message_text(text=f"네, 알겠습니다!")
+
+    if choice == "1":
+        # Add a keyword to the list
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"추가할 키워드는 무엇?")
+    elif choice == "2":
+        # Remove a selected keyword from the list
+        pass
+    elif choice == "3":
+        current_keyword(update, context)
+
+    elif choice == "4":
+        pass
+
+    else:
+        send_links(update, context)
+
+
+def send_links(update: Update, context: CallbackContext) -> None:
+    global old_links, keywords
+
+    for keyword in keywords:
+        updater = newsUpdater(keyword)
+        # print(f"saved links : {sorted(old_links[keyword])}")
+        new_links = updater.get_updated_news(old_links[keyword])
+        # print(f"new links : {sorted(new_links)}\n===============\n")
+
+        if new_links:
+            context.bot.sendMessage(
+                chat_id=get_chat_id(update, context),
+                text=f"=====\n{keyword} 관련 새로운 뉴스 {len(new_links)}건\n=====",
+            )
+            for link in new_links:
+                context.bot.sendMessage(chat_id=get_chat_id(update, context), text=link)
+                # bot.sendMessage(chat_id=62786931, text=link)
+        else:
+            context.bot.sendMessage(
+                chat_id=get_chat_id(update, context),
+                text=f"=====\n{keyword} 관련 새로운 뉴스 없음!\n=====",
+            )
+
+        old_links[keyword] += new_links.copy()
+        # old_links = list(set(old_links))
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Displays info on how to use the bot."""
+    update.message.reply_text("/hey 입력하면 시작할 수 있음")
+    print(old_links)
 
 
 # def handler(msg):
@@ -73,8 +209,28 @@ def send_links():
 #                     bot.sendMessage(chat_id, text=link)
 
 
+def main() -> None:
+    """
+    Run the bot
+    """
+    updater = Updater(TOKEN)
+
+    updater.dispatcher.add_handler(CommandHandler("hey", start))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    updater.dispatcher.add_handler(
+        MessageHandler(Filters.text & ~Filters.command, add_keyword)
+    )
+    updater.dispatcher.add_handler(CommandHandler("help", help_command))
+
+    updater.start_polling()
+
+    updater.idle()
+
+
 if __name__ == "__main__":
-    send_links()
+    main()
+
+    # send_links()
     # 스케쥴러 세팅 및 작동
-    sched.add_job(send_links, "interval", minutes=10)
-    sched.start()
+    # sched.add_job(send_links, "interval", minutes=10)
+    # sched.start()
